d92da502c38a2aa0aba3498500662439ad6e499a
Ethan Diosana <ethandiosana1@gmail.com>
2024-03-07 14:51:31 -0800

    Moved database stuff to utils

---
+++ server2/utils/database.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const databaseConfig = require('./databaseConfig.json');
+
+const pool  = mysql.createPool(databaseConfig);
+
+
+/**
+ * Performs an SQL query.
+ * @param {string} query The query to perform.
+ */
+async function performQuery(query) {
+    // Holds array of returned results.
+    let returnResults = [];     
+    if (!query) {
+        throw new Error(`query is ${query}`);
+    }
+
+    await pool.query(
+        query,
+        (error, results, fields) => {
+            if (error) throw error;
+            console.log(results);
+            returnResults = results;
+        }
+    );
+    
+    return returnResults;
+}
+
+module.exports = {
+    performQuery
+}
+
