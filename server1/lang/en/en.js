987a6ee8558a95fed4b9a59d788caa11662edd6f
Soultey <bcitalexgibbison@gmail.com>
2024-03-07 22:18:06 -0800

    en

---
+++ server1/lang/en/en.js
@@ -0,0 +1,7 @@
+/** Holds user-facing messages. */
+export const USER_MESSAGES = {
+    'textPrompt': 'Please add your CQUIL QUEERIE SLAY.',
+    'title': 'Lab 5',
+    'postLabel': 'POST',
+    'getLabel': 'GET',
+};
